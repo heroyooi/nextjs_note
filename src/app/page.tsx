fbeaf709f85a5330458bb955154fce2c2bfbae39
heroyooi <heroyooi1018@gmail.com>
2023-07-24 23:06:16 +0900

    6.5 이미지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Counter from '@/components/Counter';
+import Image from 'next/image';
 import os from 'os'; // 노드 APIs
 
 export default function Home() {
@@ -9,6 +10,12 @@ export default function Home() {
     <>
       <h1>홈페이지다!!</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={400}
+        height={400}
+      />
     </>
   );
 }
