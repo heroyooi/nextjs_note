ef21d07b0c86c3f7986d9e3cda13d201871fe9fe
heroyooi <heroyooi1018@gmail.com>
2023-07-25 14:57:03 +0900

    버전2

---
+++ src/app/about/me/ellie/page.tsx
@@ -0,0 +1,3 @@
+export default function Ellie() {
+  return <h1>나는 엘리임😍</h1>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import styles from "./layout.module.css";
-import Link from "next/link";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import styles from './layout.module.css';
+import Link from 'next/link';
+import { Open_Sans } from 'next/font/google';
+import { Nanum_Gothic } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({
+  weight: '700',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: "멋진 제품 사이트 | 전체 제품 확인",
-  description: "멋진 제품을 확인해 보세요.",
+  title: '멋진 제품 사이트 | 전체 제품 확인',
+  description: '멋진 제품을 확인해 보세요.',
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
 };
 
@@ -20,14 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en' className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h2>Demo Note App</h2>
+          <h1 className={gothic.className}>
+            <Link href='/'>Demo Note</Link>
+          </h1>
           <nav className={styles.nav}>
-            <Link href="/products">Products</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
+            <Link href='/products'>Products</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/contact'>Contact</Link>
           </nav>
         </header>
         {children}
